3cb7a3466dac2afcfae459e1f751cdb2e2f149a2
Joseph Kim <josephkim0224@gmail.com>
2018-03-14 08:52:57 -0400

    test textinput

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default class App extends Component {
+
+  state = {
+    placeName: ''
+  };
+
+  placeNameChangedHandler = (val) => {
+    this.setState({
+      placeName: val
+    });
+  };
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Testing</Text>
+        <TextInput 
+          style={{width: 300, borderColor: "black", borderWidth: 1}}
+          value={this.state.placeName}
+          onChangeText={this.placeNameChangedHandler}
+        />
       </View>
     );
   }
