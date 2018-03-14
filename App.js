101a1a5670ca1e916e9689ba8c3f04dfc9e4008b
Joseph Kim <josephkim0224@gmail.com>
2018-03-14 10:06:58 -0400

    add flatlist

--- App.js
+++ App.js
@@ -13,16 +13,19 @@ export default class App extends Component {
   placeAddedHandler = (placeName) => {
     this.setState(prevState => {
       return {
-        places: prevState.places.concat(placeName)
+        places: prevState.places.concat({
+          key: Math.random(), 
+          value: placeName
+        })
       };
     });
   };
 
-  placeDeletedHandler = index => {
+  placeDeletedHandler = key => {
     this.setState(prevState => {
       return {
-        places: prevState.places.filter((place, i) => {
-          return i !== index;
+        places: prevState.places.filter(place => {
+          return place.key !== key;
         })
       };
     });
@@ -34,7 +37,7 @@ export default class App extends Component {
         <PlaceInput onPlaceAdded={this.placeAddedHandler} />
         <PlaceList 
           places={this.state.places} 
-          onItemDelete={this.placeDeletedHandler}  
+          onItemDeleted={this.placeDeletedHandler}  
         />
       </View>
     );
