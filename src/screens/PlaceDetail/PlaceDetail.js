cff1c537af9d2cca24a86a79e27d79cf89be5c76
Joseph Kim <josephkim0224@gmail.com>
2018-03-15 16:29:58 -0400

    connect delete functionality

--- src/screens/PlaceDetail/PlaceDetail.js
+++ src/screens/PlaceDetail/PlaceDetail.js
@@ -1,32 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, Image, Text, Button, StyleSheet, TouchableOpacity } from 'react-native';
+import { connect } from 'react-redux';
+
+import { deletePlace } from '../../store/actions/index';
 
 import Icon from 'react-native-vector-icons/Ionicons';
 
-const placeDetail = props => {
-    return (
-        <View style={styles.container}>
-            <View>
-                <Image 
-                    style={styles.placeImage}
-                    source={props.selectedPlace.image}
-                />
-                <Text style={styles.placeName}>{props.selectedPlace.name}</Text>
+class PlaceDetail extends Component {
+
+    placeDeletedHandler = () => {
+        this.props.onDeletePlace(this.props.selectedPlace.key);
+        this.props.navigator.pop();
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <View>
+                    <Image 
+                        style={styles.placeImage}
+                        source={this.props.selectedPlace.image}
+                    />
+                    <Text style={styles.placeName}>{this.props.selectedPlace.name}</Text>
+                </View>
+                <View>
+                    <TouchableOpacity onPress={this.placeDeletedHandler}>
+                        <View style={styles.deleteButton}>
+                            <Icon 
+                                size={30}
+                                name="ios-trash"
+                                color="red"
+                            />
+                        </View>
+                    </TouchableOpacity>
+                </View>
             </View>
-            <View>
-                <TouchableOpacity onPress={props.onItemDeleted}>
-                    <View style={styles.deleteButton}>
-                        <Icon 
-                            size={30}
-                            name="ios-trash"
-                            color="red"
-                        />
-                    </View>
-                </TouchableOpacity>
-            </View>
-        </View>
-    );
-};
+        );
+    }    
+}
 
 const styles = StyleSheet.create({
     container: {
@@ -46,4 +57,10 @@ const styles = StyleSheet.create({
     }
 });
 
-export default placeDetail;
+const mapDispatchToProps = dispatch => {
+    return {
+        onDeletePlace: (key) => dispatch(deletePlace(key))
+    };
+};
+
+export default connect(null, mapDispatchToProps)(PlaceDetail);
--- src/store/reducers/places.js
+++ src/store/reducers/places.js
@@ -21,9 +21,8 @@ const reducer = (state = initialState, action) => {
             return {
                 ...state,
                 places: state.places.filter(place => {
-                    return place.key !== state.selectedPlace.key;
-                }),
-                selectedPlace: null
+                    return place.key !== action.placeKey;
+                })
             }
         default:
             return state;
