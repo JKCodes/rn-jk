c3115e4bc844d3c02cbee1f014aec782a16bfd59
Joseph Kim <josephkim0224@gmail.com>
2018-03-15 16:03:41 -0400

    connect app back on to Redux, and reconnect find and share places using navigation

--- index.js
+++ index.js
@@ -4,9 +4,6 @@ import App from './App';
 // import { AppRegistry } from 'react-native';
 // import { Provider } from 'react-redux';
 // import App from './App';
-// import configureStore from './src/store/configureStore';
-
-// const store = configureStore();
 
 // const RNRedux = () => (
 //     <Provider store={store}>
