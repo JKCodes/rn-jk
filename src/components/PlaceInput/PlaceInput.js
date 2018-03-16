892f0fed509ba95dc09b8bc13becfe55bae83fb2
Joseph Kim <josephkim0224@gmail.com>
2018-03-16 10:54:01 -0400

    Refactor SharePlace:

--- src/components/PlaceInput/PlaceInput.js
+++ src/components/PlaceInput/PlaceInput.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import { View, TextInput, Button, StyleSheet } from 'react-native';
+import { View, TextInput, Button } from 'react-native';
+
+import DefaultInput from '../UI/DefaultInput/DefaultInput';
 
 class PlaceInput extends Component {
 
@@ -13,46 +15,15 @@ class PlaceInput extends Component {
         });
     };
 
-    placeSubmitHandler = () => {
-        if (this.state.placeName.trim() === "") {
-          return;
-        }
-    
-        this.props.onPlaceAdded(this.state.placeName);
-    };
-
     render() {
         return (
-            <View style={styles.inputContainer}>
-                <TextInput 
-                    style={styles.placeInput}
-                    placeholder="An awesome place"
-                    value={this.state.placeName}
-                    onChangeText={this.placeNameChangedHandler}
-                />
-                <Button 
-                    style={styles.placeButton}
-                    title="Add"
-                    onPress={this.placeSubmitHandler}
-                />
-            </View>
+            <DefaultInput 
+                placeholder="Place Name" 
+                value={this.state.placeName} 
+                onChangeText={this.placeNameChangedHandler}
+            />
         );
     }
 }
-
-const styles = StyleSheet.create({
-    inputContainer: {
-      width: "100%",
-      flexDirection: "row",
-      justifyContent: "space-between",
-      alignItems: "center"
-    },
-    placeInput: {
-      width: "70%"
-    },
-    placeButton: {
-      width: "30%"
-    }
-});
 
 export default PlaceInput;
