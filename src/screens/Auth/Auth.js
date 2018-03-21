bc0316637faf49f4c5690fcba6ae848829d424fe
Joseph Kim <josephkim0224@gmail.com>
2018-03-21 17:10:41 -0400

    add change listeners for login input field

--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -9,7 +9,30 @@ import ButtonWithBackground from '../../components/UI/ButtonWithBackground/Butto
 
 class AuthScreen extends Component {
     state = {
-        viewMode: Dimensions.get("window").height > Dimensions.get("window").width ? "portrait" : "landscape"
+        viewMode: Dimensions.get("window").height > Dimensions.get("window").width ? "portrait" : "landscape",
+        controls: {
+            email: {
+                value: "",
+                valid: false,
+                validationRules: {
+                    isEmail: false
+                }
+            },
+            password: {
+                value: "",
+                valid: false,
+                validationRules: {
+                    minLength: 6
+                }
+            },
+            confirmPassword: {
+                value: "",
+                valid: false,
+                validationRules: {
+                    equalTo: "password"
+                }
+            }
+        }
     };
 
     constructor(props) {
@@ -29,6 +52,20 @@ class AuthScreen extends Component {
 
     loginHandler = () => {
         startMainTabs();
+    }
+
+    updateInputState = (key, value) => {
+        this.setState(prevState => {
+            return {
+                controls: {
+                    ...prevState.controls,
+                    [key]: {
+                        ...prevState.controls[key],
+                        value: value
+                    }
+                }
+            };
+        });
     }
 
     render() {
@@ -59,6 +96,8 @@ class AuthScreen extends Component {
                         <DefaultInput
                             placeholder="Email Address"
                             style={styles.input}
+                            value={this.state.controls.email.value}
+                            onChangeText={(val) => this.updateInputState('email', val)}
                         />
                         <View 
                             style={
@@ -77,6 +116,8 @@ class AuthScreen extends Component {
                                 <DefaultInput
                                     placeholder="Password"
                                     style={styles.input}
+                                    value={this.state.controls.password.value}
+                                    onChangeText={(val) => this.updateInputState('password', val)}
                                 />
                             </View>
                             <View 
@@ -89,6 +130,8 @@ class AuthScreen extends Component {
                                 <DefaultInput
                                     placeholder="Confirm Password"
                                     style={styles.input}
+                                    value={this.state.controls.confirmPassword.value}
+                                    onChangeText={(val) => this.updateInputState('confirmPassword', val)}
                                 />
                             </View>
                         </View>
