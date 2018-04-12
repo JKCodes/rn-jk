f9fe794c620c06238cc45333605b4e4fdad394e4
Joseph Kim <josephkim0224@gmail.com>
2018-04-12 12:46:34 -0400

    add image picker" ;

---
+++ ios/build/Build/Intermediates.noindex/RNImagePicker.build/Debug-iphonesimulator/RNImagePicker.build/Objects-normal/x86_64/ImagePickerManager.d
@@ -0,0 +1,16 @@
+dependencies: \
+  /Users/joe/Desktop/Projects/rn-jk/node_modules/react-native-image-picker/ios/ImagePickerManager.m \
+  /Users/joe/Desktop/Projects/rn-jk/node_modules/react-native-image-picker/ios/ImagePickerManager.h \
+  /Users/joe/Desktop/Projects/rn-jk/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTBridgeModule.h \
+  /Users/joe/Desktop/Projects/rn-jk/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTDefines.h \
+  /Users/joe/Desktop/Projects/rn-jk/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTConvert.h \
+  /Users/joe/Desktop/Projects/rn-jk/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTAnimationType.h \
+  /Users/joe/Desktop/Projects/rn-jk/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTBorderStyle.h \
+  /Users/joe/Desktop/Projects/rn-jk/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTLog.h \
+  /Users/joe/Desktop/Projects/rn-jk/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTAssert.h \
+  /Users/joe/Desktop/Projects/rn-jk/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTUtils.h \
+  /Users/joe/Desktop/Projects/rn-jk/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTPointerEvents.h \
+  /Users/joe/Desktop/Projects/rn-jk/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTTextDecorationLineType.h \
+  /Users/joe/Desktop/Projects/rn-jk/ios/build/Build/Products/Debug-iphonesimulator/include/yoga/Yoga.h \
+  /Users/joe/Desktop/Projects/rn-jk/ios/build/Build/Products/Debug-iphonesimulator/include/yoga/YGEnums.h \
+  /Users/joe/Desktop/Projects/rn-jk/ios/build/Build/Products/Debug-iphonesimulator/include/yoga/YGMacros.h
